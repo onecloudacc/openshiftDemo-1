8d85138ee4daaaec36750d771b13755d58f00c08
onecloudacc <onecloudacc@gmail.com>
2019-12-09 13:44:12 +0530

    Spring boot project push

---
+++ src/main/java/com/openshift/v1/OpenshiftDemo1Application.java
@@ -0,0 +1,13 @@
+package com.openshift.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftDemo1Application.class, args);
+	}
+
+}
